c05568af9a18551d7b33effad74209f15ec83a4f
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-11 14:32:27 +0200

    simplified LoginPage HTML by moving style to vars + reformat file

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from "react";
 
 const LoginPage = () => {
+  const inputContainerStyle =
+    "rounded-md m-2 pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:outline-indigo-600";
+  const inputFieldStyle = "block py-1.5 pl-1 text-gray-900 focus:outline-none sm:text-sm/6";
+  const submitContainerStyle = "m-2 flex items-center justify-end gap-x-6";
+  const submitButtonStyle =
+    "rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-md hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
+
   return (
     <form>
-      <div className="space-y-12">
-        <div className="border-gray-900/10 pb-5">
+      <div>
+        <div>
           <h1>Login</h1>
-          <div className="flex items-center rounded-md bg-white m-2 pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-            <input type="email" placeholder='email' className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
+          <div className={inputContainerStyle}>
+            <input type="email" placeholder="email" className={inputFieldStyle} required />
           </div>
-          <div className="flex items-center rounded-md bg-white m-2 pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-            <input type="password" placeholder='password' className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-gray-900 placeholder:text-gray-400 focus:outline-none sm:text-sm/6" required/>
+          <div className={inputContainerStyle}>
+            <input type="password" placeholder="password" className={inputFieldStyle} required />
           </div>
-          <div className="m-2 flex items-center justify-end gap-x-6">
-            <button type="submit" className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-md hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">login</button>
+          <div className={submitContainerStyle}>
+            <button type="submit" className={submitButtonStyle}>
+              login
+            </button>
           </div>
         </div>
       </div>
     </form>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
