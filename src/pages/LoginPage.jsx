440f0c2e84b846fac5080a2f5790f1608d88edbc
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-11 14:42:02 +0200

    added login title, adjusted styling and removed not needed tags

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 
 const LoginPage = () => {
+  const tileStyle = "mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900";
   const inputContainerStyle =
-    "rounded-md m-2 pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:outline-indigo-600";
-  const inputFieldStyle = "block py-1.5 pl-1 text-gray-900 focus:outline-none sm:text-sm/6";
-  const submitContainerStyle = "m-2 flex items-center justify-end gap-x-6";
+    "w-75 rounded-md m-3 pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:outline-indigo-600";
+  const inputFieldStyle = " block py-1.5 pl-1 text-gray-900 focus:outline-none sm:text-sm/6";
+  const submitContainerStyle = "mt-5 mb-5 flex items-center justify-center gap-x-6";
   const submitButtonStyle =
-    "rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-md hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
+    "rounded-md w-75 bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-md hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600";
 
   return (
-    <form>
-      <div>
+    <>
+      <h2 className={tileStyle}>Log in to your account</h2>
+      <form className="flex items-center justify-center">
         <div>
-          <h1>Login</h1>
           <div className={inputContainerStyle}>
             <input type="email" placeholder="email" className={inputFieldStyle} required />
           </div>
@@ -25,8 +26,8 @@ const LoginPage = () => {
             </button>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </>
   );
 };
 
