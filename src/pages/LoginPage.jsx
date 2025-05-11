ef088c0f0b77f58e79fe152f1b78bc3860cc8f6e
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-11 14:08:20 +0200

    added initial HTML content to LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <div>
+      <form>
+        <h1>Login</h1>
+        <input type="email" placeholder='email' required/>
+        <input type="password" placeholder='password' required/>
+        <button type="submit">login</button>
+      </form>
+    </div>
   )
 }
 
