568da4b4bc32471618cc5348eb1b34d55f3c750f
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-13 20:22:23 +0200

    wrapped data page in user access component in app

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,13 +3,21 @@ import PageLayout from "../layouts/PageLayout";
 import ErrorPage from "../pages/ErrorPage";
 import LoginPage from "../pages/LoginPage";
 import DataPage from "../pages/DataPage";
+import UserAccess from "./UserAccess";
 
 const Application = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<PageLayout />}>
         <Route index element={<LoginPage />} />
-        <Route path="/data" element={<DataPage />} />
+        <Route
+          path="/data"
+          element={
+            <UserAccess>
+              <DataPage />
+            </UserAccess>
+          }
+        />
         <Route path="*" element={<ErrorPage />} />
       </Route>
     )
