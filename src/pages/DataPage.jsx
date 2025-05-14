21178aabc36f7ac125f125b3e2f18eab5c16a700
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-15 00:04:21 +0200

    added logout success toast display

--- src/pages/DataPage.jsx
+++ src/pages/DataPage.jsx
@@ -1,4 +1,5 @@
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const DataPage = () => {
   const navigate = useNavigate();
@@ -6,6 +7,7 @@ const DataPage = () => {
   const userLogout = async () => {
     localStorage.removeItem("token");
     navigate("/", { replace: true });
+    toast.success("Logout successful!");
   };
 
   return (
