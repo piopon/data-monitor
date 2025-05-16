da998ae7891f79b22abbc6fbc273d5fe0e3069be
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-16 18:42:48 +0200

    added logo to header

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 
+import logo from "../assets/images/logo-64_outline.png"
+
 const PageLayout = () => {
   return (
     <>
       <nav className="bg-indigo-600 border-b border-indigo-500">
         <div className="flex h-20 items-center justify-between mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
           <a className="flex flex-shrink-0 items-center mr-4" href="/">
+            <img className="h-10 w-auto" src={logo} alt="Data Monitor" />
             <span className="hidden md:block text-white text-2xl font-bold ml-2">Data Monitor</span>
           </a>
         </div>
