26254e7a06294dd1227db298f05463ec886d8f0c
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-16 18:33:42 +0200

    added simple nav bard as the header in page layout

--- src/layouts/PageLayout.jsx
+++ src/layouts/PageLayout.jsx
@@ -4,7 +4,13 @@ import { ToastContainer } from "react-toastify";
 const PageLayout = () => {
   return (
     <>
-      <div>HEADER</div>
+      <nav className="bg-indigo-600 border-b border-indigo-500">
+        <div className="flex h-20 items-center justify-between mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <a className="flex flex-shrink-0 items-center mr-4" href="/">
+            <span className="hidden md:block text-white text-2xl font-bold ml-2">Data Monitor</span>
+          </a>
+        </div>
+      </nav>
       <Outlet />
       <div>FOOTER</div>
       <ToastContainer position="bottom-right" />
