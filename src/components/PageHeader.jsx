17ab3aa2f3747058268b50a3171157de87dfd47a
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-16 18:56:57 +0200

    reformatted newly added component code

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -1,16 +1,16 @@
-import logo from "../assets/images/logo-64_outline.png"
+import logo from "../assets/images/logo-64_outline.png";
 
 const PageHeader = () => {
   return (
     <nav className="bg-indigo-600 border-b border-indigo-500">
-        <div className="flex h-20 items-center justify-between mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <a className="flex flex-shrink-0 items-center mr-4" href="/">
-                <img className="h-10 w-auto" src={logo} alt="Data Monitor" />
-                <span className="hidden md:block text-white text-2xl font-bold ml-2">Data Monitor</span>
-            </a>
-        </div>
+      <div className="flex h-20 items-center justify-between mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <a className="flex flex-shrink-0 items-center mr-4" href="/">
+          <img className="h-10 w-auto" src={logo} alt="Data Monitor" />
+          <span className="hidden md:block text-white text-2xl font-bold ml-2">Data Monitor</span>
+        </a>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default PageHeader
+export default PageHeader;
