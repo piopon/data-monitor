3cfd655f401959162c09e69a8d6713ab4d0e9dfe
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-16 19:16:27 +0200

    restructurized index.css by dividing styles per page & common

--- src/components/PageHeader.jsx
+++ src/components/PageHeader.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/images/logo-64_outline.png";
 
 const PageHeader = () => {
   return (
-    <nav className="bg-indigo-600 border-b border-indigo-500">
+    <nav className="page-header">
       <div className="flex h-20 items-center justify-between mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <Link className="flex flex-shrink-0 items-center mr-4" to="/">
           <img className="h-10 w-auto" src={logo} alt="data-monitor logo" />
