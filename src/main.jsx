807dfae0aafd38c27bab8bd965c8758af471786c
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-19 00:12:55 +0200

    wrapped main application component with login provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Application from './components/Application.jsx'
+import LoginProvider from './context/LoginProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Application />
-  </StrictMode>,
-)
+    <LoginProvider>
+      <Application />
+    </LoginProvider>
+  </StrictMode>
+);
