86bd61a0bd0157094721c0481ef447beca70b91f
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-19 00:21:46 +0200

    added user logged boolean to login provider

--- src/context/LoginProvider.jsx
+++ src/context/LoginProvider.jsx
@@ -1,16 +1,21 @@
+import { useState } from "react";
 import { LoginContext } from "./Contexts";
 
 const LoginProvider = ({ children }) => {
+  const [userLogged, setUserLogged] = useState(() => !!localStorage.getItem("token"));
+
   const login = (token) => {
     localStorage.setItem("token", token);
+    setUserLogged(true);
   };
 
   const logout = () => {
     localStorage.removeItem("token");
+    setUserLogged(false);
   };
 
   return (
-    <LoginContext.Provider value={{ login, logout }}>
+    <LoginContext.Provider value={{ userLogged, login, logout }}>
       {children}
     </LoginContext.Provider>
   );
