3908c8181e916e76a1ad2ac337e82382edd83677
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-19 00:25:52 +0200

    reformatted main.jsx and reorganized imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import Application from './components/Application.jsx'
-import LoginProvider from './context/LoginProvider.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Application from "./components/Application.jsx";
+import LoginProvider from "./context/LoginProvider.jsx";
 
-createRoot(document.getElementById('root')).render(
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <LoginProvider>
       <Application />
