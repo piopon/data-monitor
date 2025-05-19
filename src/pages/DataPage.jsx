0975247185efc0613bc0cb9eb8424f1153e252d8
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-20 00:16:59 +0200

    removed not needed logout button from data page

--- src/pages/DataPage.jsx
+++ src/pages/DataPage.jsx
@@ -1,30 +1,6 @@
-import { useContext } from "react";
-import { useNavigate } from "react-router-dom";
-import { LoginContext } from "../context/Contexts";
-import { toast } from "react-toastify";
-
 const DataPage = () => {
-  const navigate = useNavigate();
-  const { logout } = useContext(LoginContext);
-
-  const userLogout = async (event) => {
-    event.preventDefault();
-    logout();
-    navigate("/", { replace: true });
-    toast.success("Logout successful!");
-  };
-
   return (
-    <>
-      <section id="logout-section">
-        <form className="logout-form" onSubmit={userLogout}>
-          <div className="logout-submit-div">
-            <button type="submit" className="logout-submit">logout</button>
-          </div>
-        </form>
-      </section>
       <section id="data-section">DataPage</section>
-    </>
   );
 };
 
