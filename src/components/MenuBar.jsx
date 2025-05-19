eb5c9e4312410fdcfbc2fdf435203cd4fd4ac3bb
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-20 00:16:15 +0200

    added logout button UI to menu bar

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,5 +1,13 @@
 const MenuBar = () => {
-  return <div className="page-head-menu-div">MenuBar</div>;
+  return <div className="page-head-menu-div">
+      <section id="logout-section">
+        <form className="logout-form">
+          <div className="logout-submit-div">
+            <button type="submit" className="logout-submit">logout</button>
+          </div>
+        </form>
+      </section>
+  </div>;
 };
 
 export default MenuBar;
