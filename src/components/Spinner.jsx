f4885d8bea4dd26086451d1f39513130d4187987
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-23 00:12:49 +0200

    styled and resized clip loader element

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,7 @@
 import { ClipLoader } from "react-spinners";
 
 const Spinner = ({ loading }) => {
-  return <ClipLoader loading={loading} />;
+  return <ClipLoader loading={loading} size="50" color="var(--color-indigo-600)" />;
 };
 
 export default Spinner;
