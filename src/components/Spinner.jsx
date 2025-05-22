f340a9f397c3ee68e745efe63baea1168ee9b3a4
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-23 00:13:49 +0200

    wrapped loader in a divider and added loading text

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,7 +1,12 @@
 import { ClipLoader } from "react-spinners";
 
 const Spinner = ({ loading }) => {
-  return <ClipLoader loading={loading} size="50" color="var(--color-indigo-600)" />;
+  return (
+    <div>
+      <ClipLoader loading={loading} size="50" speedMultiplier="1.1" color="var(--color-indigo-600)" />
+      <p>loading...</p>
+    </div>
+  );
 };
 
 export default Spinner;
