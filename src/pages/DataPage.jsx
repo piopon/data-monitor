57a7c5dc1235c4d9891db26485efcea9eda8ecbf
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-22 22:21:07 +0200

    fetching data for logged user in DataPage component

--- src/pages/DataPage.jsx
+++ src/pages/DataPage.jsx
@@ -1,5 +1,23 @@
+import { useEffect } from "react";
+
 const DataPage = () => {
-  return <section id="data-section">DataPage</section>;
+  useEffect(() => {
+    const getData = async () => {
+      const response = await fetch("/scraper/api/v1/data", {
+        method: "GET",
+        headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+      });
+      const data = await response.json();
+      console.log(data)
+    };
+    getData();
+  }, []);
+
+  return (
+    <section id="data-section">
+      <p>DataPage</p>
+    </section>
+  );
 };
 
 export default DataPage;
