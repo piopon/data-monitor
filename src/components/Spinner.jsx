2fa58677ce2d981f7f12bf3c27700027135da685
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-23 22:51:48 +0200

    fixed console warning by explicitly defining ClipLoader size to 50px

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -3,7 +3,7 @@ import { ClipLoader } from "react-spinners";
 const Spinner = ({ loading }) => {
   return (
     <div>
-      <ClipLoader loading={loading} size="50" speedMultiplier="1.1" color="var(--color-indigo-600)" />
+      <ClipLoader loading={loading} size="50px" speedMultiplier="1.1" color="var(--color-indigo-600)" />
       <p className="spinner-loading">loading...</p>
     </div>
   );
