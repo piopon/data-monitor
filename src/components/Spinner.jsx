c9c487437d753b206bf93067d17b4f7617f770d9
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-24 00:13:24 +0200

    bugfix: correctly centered spinner after latest adjustments

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,7 +2,7 @@ import { ClipLoader } from "react-spinners";
 
 const Spinner = ({ loading }) => {
   return (
-    <div>
+    <div className="w-full h-full flex flex-col justify-center items-center">
       <ClipLoader loading={loading} size="50px" speedMultiplier="1.1" color="var(--color-indigo-600)" />
       <p className="spinner-loading">loading...</p>
     </div>
