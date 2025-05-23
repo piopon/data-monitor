59bc7e09877a9ec0a466cd82309690f0f91fcf27
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-23 22:49:15 +0200

    wrapped data card title to a div and placed items in another

--- src/components/DataCards.jsx
+++ src/components/DataCards.jsx
@@ -4,8 +4,15 @@ const DataCards = ({ data }) => {
       {data.map((item) => (
         <div class="bg-white rounded-xl shadow-lg relative">
           <div class="p-4">
-            <div class="text-gray-600 my-2">category: {item.category}</div>
-            <h3 class="text-xl font-bold">{item.name}</h3>
+            <div class="mb-2">
+              <div class="text-gray-400 my-2">category: {item.category}</div>
+              <h3 class="text-xl font-bold">{item.name}</h3>
+            </div>
+            <div class="mb-6">
+              {item.items.map((element) => (
+                <p class="text-md">{element.name}: {element.price} {element.currency}</p>
+              ))}
+            </div>
           </div>
         </div>
       ))}
