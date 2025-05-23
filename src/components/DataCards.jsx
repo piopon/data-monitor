b7698a0653cb1567446bcfa44a8b7dffe0e132a3
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-23 22:39:37 +0200

    added HTML code for data card title content

--- src/components/DataCards.jsx
+++ src/components/DataCards.jsx
@@ -2,9 +2,12 @@ const DataCards = ({ data }) => {
   return (
     <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
       {data.map((item) => (
-        <p>
-          {item.name} - {item.category} - {item.items.length} item(s)
-        </p>
+        <div class="bg-white rounded-xl shadow-lg relative">
+          <div class="p-4">
+            <div class="text-gray-600 my-2">category: {item.category}</div>
+            <h3 class="text-xl font-bold">{item.name}</h3>
+          </div>
+        </div>
       ))}
     </div>
   );
