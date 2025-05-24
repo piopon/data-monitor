05ac227369cc449cc766c2ab9a1948292ff35ffa
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-24 20:01:34 +0200

    removed intermediate divider in data card

--- src/components/DataCards.jsx
+++ src/components/DataCards.jsx
@@ -2,22 +2,20 @@ const DataCards = ({ data }) => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-3 gap-10">
       {data.map((item) => (
-        <div className="bg-white rounded-xl shadow-lg relative">
-          <div className="p-4">
-            <div className="mb-2">
-              <div className="text-gray-400 my-2">category: {item.category}</div>
-              <h3 className="text-xl font-bold">{item.name}</h3>
-            </div>
-            <div className="mb-6">
-              {item.items.map((element) => (
-                <div>
-                  <img src={element.icon} alt={element.name+" logo"}/>
-                  <p className="text-md">
-                    {element.name}: {element.price} {element.currency}
-                  </p>
-                </div>
-              ))}
-            </div>
+        <div className="bg-white p-4 rounded-xl shadow-lg relative">
+          <div className="mb-2">
+            <div className="text-gray-400 my-2">category: {item.category}</div>
+            <h3 className="text-xl font-bold">{item.name}</h3>
+          </div>
+          <div className="mb-6">
+            {item.items.map((element) => (
+              <div>
+                <img src={element.icon} alt={element.name+" logo"}/>
+                <p className="text-md">
+                  {element.name}: {element.price} {element.currency}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       ))}
