c5c57309983fb528479514a41708778aa3501d9b
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-26 23:36:21 +0200

    reformatted data item component

--- src/components/DataItem.jsx
+++ src/components/DataItem.jsx
@@ -3,7 +3,7 @@ import unknown from "../assets/images/unknown.png";
 const DataItem = ({ item }) => {
   const valid = "OK" === item.status;
   const state = valid ? `✔️` : `❌`;
-  const image = valid ? {src: item.icon, alt: `${item.name} logo`} : {src: unknown, alt: `Unknown logo`};
+  const image = valid ? { src: item.icon, alt: `${item.name} logo` } : { src: unknown, alt: `Unknown logo` };
   const detail = valid ? `${item.price} ${item.currency}` : item.reason;
   const config = valid ? `THRESHOLD SETTINGS...` : `FIX ITEM CONFIG...`;
 
@@ -11,9 +11,7 @@ const DataItem = ({ item }) => {
     <div className="data-card-item">
       <img src={image.src} alt={image.alt} />
       <div>
-        <p>
-          {state}<span>{item.name}</span>: {detail}
-        </p>
+        <p>{state}<span>{item.name}</span>: {detail}</p>
         <div>{config}</div>
       </div>
     </div>
