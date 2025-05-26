93c33f903274455ecadce8ecb2dea994ebe3c678
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-26 23:47:13 +0200

    used more precise object wrappers to currently existing variables

--- src/components/DataItem.jsx
+++ src/components/DataItem.jsx
@@ -4,15 +4,19 @@ const DataItem = ({ item }) => {
   const valid = "OK" === item.status;
   const state = valid ? `✔️` : `❌`;
   const image = valid ? { src: item.icon, alt: `${item.name} logo` } : { src: unknown, alt: `Unknown logo` };
-  const detail = valid ? `${item.price} ${item.currency}` : item.reason;
-  const config = valid ? `THRESHOLD SETTINGS...` : `FIX ITEM CONFIG...`;
+  const value = valid
+    ? { data: `${item.price} ${item.currency}`, threshold: `THRESHOLD SETTINGS...` }
+    : { data: item.reason, threshold: `FIX ITEM CONFIG...` };
 
   return (
     <div className="data-card-item">
       <img src={image.src} alt={image.alt} />
       <div>
-        <p>{state}<span>{item.name}</span>: {detail}</p>
-        <div>{config}</div>
+        <p>
+          {state}
+          <span>{item.name}</span>: {value.data}
+        </p>
+        <div>{value.threshold}</div>
       </div>
     </div>
   );
