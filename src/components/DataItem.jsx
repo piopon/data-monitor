2685c7a37debfefbf116aceb89771b91ab10eff5
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-26 18:21:04 +0200

    added item status check when drawing data item component

--- src/components/DataItem.jsx
+++ src/components/DataItem.jsx
@@ -1,13 +1,16 @@
 const DataItem = ({ item }) => {
-  return (
+  return "OK" === item.status ? (
     <div className="data-card-item">
       <img src={item.icon} alt={item.name + " logo"} />
       <div>
         <p>
-          <span>{item.name}</span>: {item.price} {item.currency}
+          ✔️<span>{item.name}</span>: {item.price} {item.currency}
         </p>
         <div>TRESHOLD SETTINGS...</div>
       </div>
+    </div>
+  ) : (
+    <div className="data-card-item">
     </div>
   );
 };
