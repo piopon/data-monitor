f0b8705a8563d1b587042346efc37b78e7d6c675
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-30 20:23:34 +0200

    added database setting to app config class (for future reference)

--- src/config/AppConfig.js
+++ src/config/AppConfig.js
@@ -5,6 +5,13 @@ export class AppConfig {
         host: process.env.SCRAPER_HOST || "localhost",
         port: parseInt(process.env.SCRAPER_PORT) || 5000,
       },
+      database: {
+        host: process.env.DB_HOST || "localhost",
+        name: process.env.DB_NAME || "data-monitor",
+        port: parseInt(process.env.DB_PORT) || 27017,
+        user: process.env.DB_USER || "",
+        password: process.env.DB_PASSWORD || "",
+      },
     };
   }
 }
