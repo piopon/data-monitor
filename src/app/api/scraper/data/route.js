8e5fdeebac88d7566f170fea4b0b29ba7c9b121f
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-31 12:23:49 +0200

    adjusted parameter name in both backend scraper routes

--- src/app/api/scraper/data/route.js
+++ src/app/api/scraper/data/route.js
@@ -3,12 +3,12 @@ import { ScraperRequest } from "@/lib/ScraperRequest";
 
 /**
  * Method used to send the data GET request to scraper backend service
- * @param {Object} req Request object received from the frontend
+ * @param {Object} request Request object received from the frontend
  * @returns Response object with data values
  */
-export async function GET(req) {
+export async function GET(request) {
   return ScraperRequest.GET(
     AppConfig.getConfig().scraper.endpoints.data,
-    { Authorization: req.headers.get("authorization") || "" }
+    { Authorization: request.headers.get("authorization") || "" }
   );
 }
--- src/app/api/scraper/token/route.js
+++ src/app/api/scraper/token/route.js
@@ -3,13 +3,13 @@ import { ScraperRequest } from "@/lib/ScraperRequest";
 
 /**
  * Method used to send the login POST request to scraper backend service
- * @param {Object} req Request object received from the frontend
+ * @param {Object} request Request object received from the frontend
  * @returns Response object with JWT value
  */
-export async function POST(req) {
+export async function POST(request) {
   return ScraperRequest.POST(
     AppConfig.getConfig().scraper.endpoints.login,
     { "Content-Type": "application/json" },
-    JSON.stringify(await req.json())
+    JSON.stringify(await request.json())
   );
 }
