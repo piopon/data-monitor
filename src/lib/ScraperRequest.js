907fc0492f593a7b1e0e833f6e711ebd2075051f
Piotr Ponikowski <piopon.github@gmail.com>
2025-05-31 11:57:51 +0200

    added public post and get methods to scraper request

--- src/lib/ScraperRequest.js
+++ src/lib/ScraperRequest.js
@@ -1,4 +1,18 @@
 export class ScraperRequest {
+  #scraperUrl = undefined;
+
+  constructor(scraperUrl) {
+    this.#scraperUrl = scraperUrl;
+  }
+
+  post(endpoint, headers, body) {
+    return this.#sendRequest(`${this.#scraperUrl}${endpoint}`, "POST", headers, body);
+  }
+
+  get(endpoint, headers) {
+    return this.#sendRequest(`${this.#scraperUrl}${endpoint}`, "GET", headers);
+  }
+
   async #sendRequest(url, method, headers, body = undefined) {
     const response = await fetch(url, {
       method: method,
