0cb80382800ba668f0b1be0ffa3ff3ed78407543
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-04 20:22:25 +0200

    added DB error listener which stops the server

--- src/lib/Database.js
+++ src/lib/Database.js
@@ -7,6 +7,11 @@ const db = new pg.Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
   });
+
 db.connect();
+db.on('error', (err) => {
+    console.error('Unexpected error on idle client', err);
+    process.exit(-1);
+  });
 
 export const query = (text, params) => db.query(text, params);
