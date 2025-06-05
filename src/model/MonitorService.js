0887bae561d14e9787b0438d686065f45a18a2d6
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-05 20:14:27 +0200

    updated monitor service file to have a class type (not loosely exported fn)

--- src/model/MonitorService.js
+++ src/model/MonitorService.js
@@ -1,15 +1,20 @@
 import { DatabaseQuery } from "@/lib/DatabaseQuery";
 import { Monitor } from "./Monitor";
 
-export const initialize = async () => {
-  const tableName = Monitor.getTableName();
-  try {
-    await DatabaseQuery(`
+export class MonitorService {
+  /**
+   * Method used to initialize database table for monitor data
+   */
+  async initializeDbTable() {
+    const tableName = Monitor.getTableName();
+    try {
+      await DatabaseQuery(`
       CREATE TABLE IF NOT EXISTS ${tableName} (
         ${Monitor.getDatabaseSchema()}
       );`);
-    console.log(`Initialized '${tableName}' table.`);
-  } catch (error) {
-    console.error(`Cannot initialize '${tableName}' table: ${error.message}`);
+      console.log(`Initialized '${tableName}' table.`);
+    } catch (error) {
+      console.error(`Cannot initialize '${tableName}' table: ${error.message}`);
+    }
   }
-};
+}
