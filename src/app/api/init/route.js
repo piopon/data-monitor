ff2e6fec3a13eec14259dc1bf4be101aec5d17c0
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-05 20:20:38 +0200

    upgraded api init route logic to return appropriate result + object

--- src/app/api/init/route.js
+++ src/app/api/init/route.js
@@ -1,6 +1,9 @@
 import { MonitorService } from "@/model/MonitorService";
 
 export async function GET(request) {
-  await MonitorService.initializeDbTable();
-  return new Response("DB init invoked");
+  const monitorInitResult = await MonitorService.initializeDbTable();
+  return new Response(monitorInitResult, {
+    status: monitorInitResult.result ? 200 : 500,
+    headers: { "Content-Type": "application/json" },
+  });
 }
