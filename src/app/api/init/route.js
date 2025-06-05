5e8f46b85f6a12c88ba33bb3ad87fdc8614f7011
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-05 20:19:14 +0200

    adjusted init monitor table method to be static + returned combined object

--- src/app/api/init/route.js
+++ src/app/api/init/route.js
@@ -1,6 +1,6 @@
 import { MonitorService } from "@/model/MonitorService";
 
 export async function GET(request) {
-  await new MonitorService().initializeDbTable();
+  await MonitorService.initializeDbTable();
   return new Response("DB init invoked");
 }
--- src/model/MonitorService.js
+++ src/model/MonitorService.js
@@ -5,16 +5,16 @@ export class MonitorService {
   /**
    * Method used to initialize database table for monitor data
    */
-  async initializeDbTable() {
+  static async initializeDbTable() {
     const tableName = Monitor.getTableName();
     try {
       await DatabaseQuery(`
       CREATE TABLE IF NOT EXISTS ${tableName} (
         ${Monitor.getDatabaseSchema()}
       );`);
-      console.log(`Initialized '${tableName}' table.`);
+      return { result: true, message: `Initialized '${tableName}' table.`};
     } catch (error) {
-      console.error(`Cannot initialize '${tableName}' table: ${error.message}`);
+      return { result: false, message: `Cannot initialize '${tableName}' table: ${error.message}`};
     }
   }
 }
