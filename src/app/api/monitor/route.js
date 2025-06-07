9185d827ceb7ba5f89cf11cb0c68e5ab2941c663
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-08 00:10:45 +0200

    new add monitor controller logic via API monitor POST request

--- src/app/api/monitor/route.js
+++ src/app/api/monitor/route.js
@@ -16,3 +16,20 @@ export async function GET(request) {
     });
   }
 }
+
+export async function POST(request) {
+  try {
+    const monitorData = request.body;
+    const monitor = await MonitorService.createClient(monitorData);
+    new Response(monitor, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    const errorOutput = { message: `Cannot add new monitor: ${error.message}` };
+    new Response(errorOutput, {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
