2d280c99772e726bcbb905437cbfe664bbbfc9d5
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-07 13:31:01 +0200

    added initial version of API monitor route with GET method support

---
+++ src/app/api/monitor/route.js
@@ -0,0 +1,18 @@
+import { MonitorService } from "@/model/MonitorService";
+
+export async function GET(request) {
+  try {
+    const searchParams = request.nextUrl.searchParams;
+    const monitors = MonitorService.getMonitors(searchParams ? searchParams : undefined);
+    new Response(monitors, {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    const errorOutput = { message: `Cannot get monitors: ${error.message}` };
+    new Response(errorOutput, {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
