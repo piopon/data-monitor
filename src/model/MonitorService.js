ad05fe0bd85a8a3574a554ceccfce4399a34d745
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-07 13:25:22 +0200

    added methods descriptions in model service class

--- src/model/MonitorService.js
+++ src/model/MonitorService.js
@@ -6,6 +6,7 @@ export class MonitorService {
 
   /**
    * Method used to initialize database table for monitor data
+   * @returns object with initialize result and detailed info message
    */
   static async initializeTable() {
     try {
@@ -19,11 +20,20 @@ export class MonitorService {
     }
   }
 
+  /**
+   * Method used to receive all monitors saved in database
+   * @returns array of monitor objects from database
+   */
   static async getMonitors() {
     const { rows } = await DatabaseQuery(`SELECT * FROM ${MonitorService.#DB_TABLE_NAME}`);
     return rows;
   }
 
+  /**
+   * Method used to receive monitors matching provided filter expression
+   * @param {String} filter expression used to filter monitor objects
+   * @returns array of monitor objects matching filter expression
+   */
   static async getMonitors(filter) {
     const { rows } = await DatabaseQuery(
       `SELECT * FROM ${
@@ -34,6 +44,11 @@ export class MonitorService {
     return rows;
   }
 
+  /**
+   * Method used to add provided monitor data to database
+   * @param {Object} data monitor object which we want to add to database
+   * @returns added monitor object
+   */
   static async addMonitor(data) {
     const { parent, enable, threshold, condition, notifier } = data;
     const { rows } = await DatabaseQuery(
@@ -45,6 +60,12 @@ export class MonitorService {
     return rows[0];
   }
 
+  /**
+   * Method used to edit monitor data in the database
+   * @param {Number} id database identifier which we want to edit
+   * @param {Object} data monitor object which we want to add to database
+   * @returns updated monitor object
+   */
   static async editMonitor(id, data) {
     const { parent, enable, threshold, condition, notifier } = data;
     const { rows } = await DatabaseQuery(
@@ -56,6 +77,11 @@ export class MonitorService {
     return rows[0];
   }
 
+  /**
+   * Method used to delete monitor data from the database
+   * @param {Number} id database identifier which we want to remove
+   * @returns number of deleted monitor object(s)
+   */
   static async deleteMonitor(id) {
     const { rowCount } = await DatabaseQuery(`DELETE FROM ${MonitorService.#DB_TABLE_NAME} WHERE id = $1`, [id]);
     return rowCount > 0;
