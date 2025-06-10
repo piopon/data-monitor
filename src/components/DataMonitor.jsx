178c9610079d2ce7d92c90ebbb6b0978a0f19a0d
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-10 20:07:05 +0200

    added save button used to store monitor data in DB

--- src/components/DataMonitor.jsx
+++ src/components/DataMonitor.jsx
@@ -1,18 +1,21 @@
 const DataMonitor = ({parent}) => {
   return (
     <div className="data-card-monitor">
-      <input type="checkbox" name={`${parent}-enabled`} />
-      <select name="condition">
-        <option value="<">&lt;</option>
-        <option value="<=">&le;</option>
-        <option value=">">&gt;</option>
-        <option value=">=">&ge;</option>
-      </select>
-      <input type="text" placeholder="threshold"/>
-      <select name="notifier">
-        <option value="email">email</option>
-        <option value="discord">discord</option>
-      </select>
+      <form>
+        <input type="checkbox" name={`${parent}-enabled`} />
+        <select name="condition">
+          <option value="<">&lt;</option>
+          <option value="<=">&le;</option>
+          <option value=">">&gt;</option>
+          <option value=">=">&ge;</option>
+        </select>
+        <input type="text" placeholder="threshold"/>
+        <select name="notifier">
+          <option value="email">email</option>
+          <option value="discord">discord</option>
+        </select>
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
