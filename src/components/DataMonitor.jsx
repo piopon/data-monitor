a3bb92d2b1ae9ae81f2d7bcdf279132e17d0538c
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-11 20:29:25 +0200

    added state with all monitor fields to DataMonitor component

--- src/components/DataMonitor.jsx
+++ src/components/DataMonitor.jsx
@@ -1,16 +1,23 @@
+import { useState } from "react";
+
 const DataMonitor = ({ parent }) => {
+  const [enabled, setEnabled] = useState(false);
+  const [threshold, setThreshold] = useState("");
+  const [condition, setCondition] = useState("<");
+  const [notifier, setNotifier] = useState("discord");
+
   return (
     <div className="data-card-monitor">
       <form>
-        <input type="checkbox" name={`${parent}-enabled`} />
-        <select name="condition">
+        <input type="checkbox" name={`${parent}-enabled`} checked={enabled} />
+        <select name="condition" value={condition}>
           <option value="<">&lt;</option>
           <option value="<=">&le;</option>
           <option value=">">&gt;</option>
           <option value=">=">&ge;</option>
         </select>
-        <input type="text" placeholder="threshold" />
-        <select name="notifier">
+        <input type="text" placeholder="threshold" value={threshold} />
+        <select name="notifier" value={notifier}>
           <option value="email">email</option>
           <option value="discord">discord</option>
         </select>
