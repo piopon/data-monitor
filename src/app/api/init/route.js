6927913aa3784393bb94ac6f81200468badd23f8
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-18 00:47:42 +0200

    corrected invalid JSON object while initializing DB table

--- src/app/api/init/route.js
+++ src/app/api/init/route.js
@@ -2,7 +2,7 @@ import { MonitorService } from "@/model/MonitorService";
 
 export async function GET(request) {
   const monitorInitResult = await MonitorService.initializeTable();
-  return new Response(monitorInitResult, {
+  return new Response(JSON.stringify(monitorInitResult), {
     status: monitorInitResult.result ? 200 : 500,
     headers: { "Content-Type": "application/json" },
   });
