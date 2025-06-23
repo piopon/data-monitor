2a24aafedddf8b83612db5a9adc2c0c40bdf3305
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-24 00:12:53 +0200

    moved monitor toggle to a separate component

--- src/components/DataMonitor.jsx
+++ src/components/DataMonitor.jsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { toast } from "react-toastify";
+import Toggle from "./Toggle";
 
 const DataMonitor = ({ parentName }) => {
   const defaults = { id: 0, enabled: false, threshold: "", condition: "<", notifier: "email" };
@@ -68,53 +69,7 @@ const DataMonitor = ({ parentName }) => {
   return (
     <div className="data-card-monitor">
       <form onSubmit={saveMonitor}>
-        <div class="flex items-center">
-          <label for="hs-medium-switch-with-icons" class="relative inline-block w-13 h-7 cursor-pointer">
-            <input
-              type="checkbox"
-              class="peer sr-only"
-              id="hs-medium-switch-with-icons"
-              name={`${parentId}-enabled`}
-              checked={enabled}
-              onChange={(event) => setEnabled(event.target.checked)}
-            />
-            <span class="absolute inset-0 bg-gray-200 rounded-full transition-colors duration-200 ease-in-out peer-checked:bg-blue-600 dark:bg-neutral-700 dark:peer-checked:bg-blue-500 peer-disabled:opacity-50 peer-disabled:pointer-events-none"></span>
-            <span class="absolute top-1/2 start-0.5 -translate-y-1/2 size-6 bg-white rounded-full shadow-xs transition-transform duration-200 ease-in-out peer-checked:translate-x-full dark:bg-neutral-400 dark:peer-checked:bg-white"></span>
-            <span class="absolute top-1/2 start-1 -translate-y-1/2 flex justify-center items-center size-5 text-gray-500 peer-checked:text-white transition-colors duration-200 dark:text-neutral-500">
-              <svg
-                class="shrink-0 size-3"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <path d="M18 6 6 18"></path>
-                <path d="m6 6 12 12"></path>
-              </svg>
-            </span>
-            <span class="absolute top-1/2 end-1 -translate-y-1/2 flex justify-center items-center size-5 text-gray-500 peer-checked:text-blue-600 transition-colors duration-200 dark:text-neutral-500">
-              <svg
-                class="shrink-0 size-3"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-width="2.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              >
-                <polyline points="20 6 9 17 4 12"></polyline>
-              </svg>
-            </span>
-          </label>
-        </div>
+        <Toggle id={parentId} enabled={enabled} setter={setEnabled} />
         <select
           class="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
           name="condition"
