4a5b94d4c14f22ae6b4b7ce3774e2bf40704da4d
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-25 00:04:21 +0200

    moved toggle background and button class defs to separate objs

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -23,8 +23,8 @@ const Toggle = ({ id, enabled, setter }) => {
           checked={enabled}
           onChange={(event) => setter(event.target.checked)}
         />
-        <span className="absolute inset-0 bg-gray-200 rounded-full transition-colors duration-200 ease-in-out peer-checked:bg-blue-600 dark:bg-neutral-700 dark:peer-checked:bg-blue-500 peer-disabled:opacity-50 peer-disabled:pointer-events-none"></span>
-        <span className="absolute top-1/2 start-0.5 -translate-y-1/2 size-6 bg-white rounded-full shadow-xs transition-transform duration-200 ease-in-out peer-checked:translate-x-full dark:bg-neutral-400 dark:peer-checked:bg-white"></span>
+        <span className="toggle-background"></span>
+        <span className="toggle-button"></span>
         <span className="toggle-icon-left">{renderIcon(ICON_DISABLED)}</span>
         <span className="toggle-icon-right">{renderIcon(ICON_ENABLED)}</span>
       </label>
