bc7bd73960a63a254bb42cf29d34ad63cb491fdf
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-24 23:22:49 +0200

    added render icon helper method with parent SVG and parameterized child

--- src/components/Toggle.jsx
+++ src/components/Toggle.jsx
@@ -1,4 +1,17 @@
 const Toggle = ({id, enabled, setter}) => {
+  const renderIcon = (child) => (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2.5"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      {child}
+    </svg>
+  );
+
   return (
     <div className="toggle">
       <label htmlFor={`${id}-toggle`} className="toggle-root">
@@ -13,30 +26,14 @@ const Toggle = ({id, enabled, setter}) => {
         <span className="absolute inset-0 bg-gray-200 rounded-full transition-colors duration-200 ease-in-out peer-checked:bg-blue-600 dark:bg-neutral-700 dark:peer-checked:bg-blue-500 peer-disabled:opacity-50 peer-disabled:pointer-events-none"></span>
         <span className="absolute top-1/2 start-0.5 -translate-y-1/2 size-6 bg-white rounded-full shadow-xs transition-transform duration-200 ease-in-out peer-checked:translate-x-full dark:bg-neutral-400 dark:peer-checked:bg-white"></span>
         <span className="toggle-icon-left">
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path d="M18 6 6 18"></path>
-            <path d="m6 6 12 12"></path>
-          </svg>
+          {renderIcon(
+            <>
+              <path d="M18 6 6 18"></path>
+              <path d="m6 6 12 12"></path>
+            </>
+          )}
         </span>
-        <span className="toggle-icon-right">
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2.5"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <polyline points="20 6 9 17 4 12"></polyline>
-          </svg>
-        </span>
+        <span className="toggle-icon-right">{renderIcon(<polyline points="20 6 9 17 4 12"></polyline>)}</span>
       </label>
     </div>
   );
