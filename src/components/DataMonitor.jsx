be8151574d695f1aa154df565f59a9ddcb729548
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-25 00:34:43 +0200

    updated notifier select input to use new Select component

--- src/components/DataMonitor.jsx
+++ src/components/DataMonitor.jsx
@@ -73,6 +73,10 @@ const DataMonitor = ({ parentName }) => {
     { value: ">", text: ">" },
     { value: "≥", text: "≥" },
   ];
+  const notifiers = [
+    { value: "email", text: "email" },
+    { value: "discord", text: "discord" },
+  ];
 
   return (
     <div className="data-card-monitor">
@@ -86,15 +90,7 @@ const DataMonitor = ({ parentName }) => {
           value={threshold}
           onChange={(event) => setThreshold(event.target.value)}
         />
-        <select
-          className="py-3 px-4 pe-9 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600"
-          name="notifier"
-          value={notifier}
-          onChange={(event) => setNotifier(event.target.value)}
-        >
-          <option value="email">email</option>
-          <option value="discord">discord</option>
-        </select>
+        <Select options={notifiers} value={notifier} setter={setNotifier} />
         <button
           className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-red-500 text-white hover:bg-red-600 focus:outline-hidden focus:bg-red-600 disabled:opacity-50 disabled:pointer-events-none"
           type="submit"
