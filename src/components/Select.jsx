920e5a11848faae87a963c4ddc9da2948a235df2
Piotr Ponikowski <piopon.github@gmail.com>
2025-06-28 00:23:36 +0200

    reformatted Select component source code

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -2,7 +2,9 @@ const Select = ({ options, value, disabled, setter }) => {
   return (
     <select value={value} disabled={disabled} onChange={(event) => setter(event.target.value)}>
       {options.map((option) => (
-        <option key={option.value} value={option.value}>{option.text}</option>
+        <option key={option.value} value={option.value}>
+          {option.text}
+        </option>
       ))}
     </select>
   );
