b54e89d754e0d81737b40def2d74f78a86774fc2
Piotr Ponikowski <piopon.github@gmail.com>
2025-07-24 23:34:56 +0200

    reformatted and cleaned up NotFound component source code

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const NotFound = () => {
-  return <p className="page-not-found"><span>404</span><br/>🚧 page not found 🚧</p>;
+  return (
+    <p className="page-not-found">
+      <span>404</span>
+      <br />
+      🚧 page not found 🚧
+    </p>
+  );
 };
 
 export default NotFound;
