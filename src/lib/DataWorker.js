5923fadee4f5912f2f34176fcb9976f980fd9160
Piotr Ponikowski <piopon.github@gmail.com>
2025-07-26 18:48:20 +0200

    fixed issue with enabled monitors not being an iterable & added temp JWT

--- src/lib/DataWorker.js
+++ src/lib/DataWorker.js
@@ -3,11 +3,12 @@ import { MonitorService } from "../model/MonitorService.js";
 const INTERVAL = 60 * 1000;
 
 async function checkData() {
-  const enabledMonitors = MonitorService.filterMonitors({ enabled: true });
+  const enabledMonitors = await MonitorService.filterMonitors({ enabled: true });
   for (const monitor of enabledMonitors) {
     try {
       const response = await fetch(`http://localhost:3000/api/scraper/data?name=${monitor.parent}`, {
         method: "GET",
+        headers: { Authorization: `Bearer ${process.env.TEMP_TOKEN}` },
       });
       if (response.items[0].price > monitor.threshold) {
         console.log(`Sending notification: ${monitor.parent} over threshold!`);
