0a0f2abbdb3f51a635bd03e8fc1e013fa73bacae
Piotr Ponikowski <piopon.github@gmail.com>
2025-07-26 18:06:58 +0200

    added try-catch to data worker script logic

--- src/lib/DataWorker.js
+++ src/lib/DataWorker.js
@@ -5,11 +5,15 @@ const INTERVAL = 60 * 1000;
 async function checkData() {
   const enabledMonitors = MonitorService.filterMonitors({ enabled: true });
   for (const monitor of enabledMonitors) {
-    const response = await fetch(`http://localhost:3000/api/scraper/data?name=${monitor.parent}`, {
-      method: "GET",
-    });
-    if (response.items[0].price > monitor.threshold) {
-      console.log(`Sending notification: ${monitor.parent} over threshold!`);
+    try {
+      const response = await fetch(`http://localhost:3000/api/scraper/data?name=${monitor.parent}`, {
+        method: "GET",
+      });
+      if (response.items[0].price > monitor.threshold) {
+        console.log(`Sending notification: ${monitor.parent} over threshold!`);
+      }
+    } catch (err) {
+      console.error("Worker error: ", err.message);
     }
   }
 }
