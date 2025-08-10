af6bb414493ed493de0abc1f8c923b33d217d707
Piotr Ponikowski <piopon.github@gmail.com>
2025-08-10 13:57:26 +0200

    added nameToId method description

--- src/lib/DataUtils.js
+++ src/lib/DataUtils.js
@@ -1,4 +1,9 @@
 export class DataUtils {
+  /**
+   * Method used to convert provided name to an ID string
+   * @param {String} name The name to be converted to an ID string
+   * @returns an identifier-compatible string from name
+   */
   static nameToId(name) {
     return name.toLowerCase().replace(/\s+/g, "-");
   }
