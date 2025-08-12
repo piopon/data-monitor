0c2732df8c09cb3236a6cd83c443e59f23505d1e
Piotr Ponikowski <piopon.github@gmail.com>
2025-08-13 00:54:04 +0200

    new add/edit user logic in service class

--- src/model/UserService.js
+++ src/model/UserService.js
@@ -25,4 +25,21 @@ export class UserService {
     const { rows } = await DatabaseQuery(`SELECT * FROM ${UserService.#DB_TABLE_NAME} WHERE id = $1`, [id]);
     return rows;
   }
+
+  static async addUser(data) {
+    const { jwt } = data;
+    const { rows } = await DatabaseQuery(`INSERT INTO ${UserService.#DB_TABLE_NAME} (jwt) VALUES ($1) RETURNING *`, [
+      jwt,
+    ]);
+    return rows[0];
+  }
+
+  static async editUser(id, data) {
+    const { jwt } = data;
+    const { rows } = await DatabaseQuery(
+      `UPDATE ${UserService.#DB_TABLE_NAME} SET jwt = $1 WHERE id = $2 RETURNING *`,
+      [jwt, id]
+    );
+    return rows[0];
+  }
 }
