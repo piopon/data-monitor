fab30fb04b64a3616d6924348a7a37c3f5ff7393
Piotr Ponikowski <piopon.github@gmail.com>
2025-08-13 23:11:31 +0200

    modified database initialization logic to support both tables

--- src/app/api/init/route.js
+++ src/app/api/init/route.js
@@ -1,9 +1,23 @@
 import { MonitorService } from "@/model/MonitorService";
+import { UserService } from "@/model/UserService";
 
 export async function GET(request) {
   const monitorInitResult = await MonitorService.initializeTable();
-  return new Response(JSON.stringify(monitorInitResult), {
-    status: monitorInitResult.result ? 200 : 500,
+  if (!monitorInitResult.result) {
+    return new Response(JSON.stringify(monitorInitResult), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  const userInitResult = await UserService.initializeTable();
+  if (!userInitResult.result) {
+    return new Response(JSON.stringify(userInitResult), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  return new Response(JSON.stringify("Database initialized correctly."), {
+    status: 200,
     headers: { "Content-Type": "application/json" },
   });
 }
