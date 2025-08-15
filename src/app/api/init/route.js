47cb2af683c91871a1f6d67296aa0f265a8b3eaa
Piotr Ponikowski <piopon.github@gmail.com>
2025-08-16 00:59:08 +0200

    bugfix: corrected tables init order (user first, then monitor)

--- src/app/api/init/route.js
+++ src/app/api/init/route.js
@@ -2,16 +2,16 @@ import { MonitorService } from "@/model/MonitorService";
 import { UserService } from "@/model/UserService";
 
 export async function GET(request) {
-  const monitorInitResult = await MonitorService.initializeTable();
-  if (!monitorInitResult.result) {
-    return new Response(JSON.stringify(monitorInitResult.message), {
+  const userInitResult = await UserService.initializeTable();
+  if (!userInitResult.result) {
+    return new Response(JSON.stringify(userInitResult.message), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
   }
-  const userInitResult = await UserService.initializeTable();
-  if (!userInitResult.result) {
-    return new Response(JSON.stringify(userInitResult.message), {
+  const monitorInitResult = await MonitorService.initializeTable();
+  if (!monitorInitResult.result) {
+    return new Response(JSON.stringify(monitorInitResult.message), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
