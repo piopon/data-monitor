9e7427e688585e200346f307a3af67a7a1a8e8cd
Piotr Ponikowski <piopon.github@gmail.com>
2025-10-07 23:22:16 +0200

    added method description to scraper/items GET method

--- src/app/api/scraper/items/route.js
+++ src/app/api/scraper/items/route.js
@@ -1,6 +1,11 @@
 import { AppConfig } from "@/config/AppConfig";
 import { ScraperRequest } from "@/lib/ScraperRequest";
 
+/**
+ * Method used to send the items GET request to scraper backend service
+ * @param {Object} request Request object received from the frontend
+ * @returns Response object with data items array values
+ */
 export async function GET(request) {
   return ScraperRequest.GET(
     AppConfig.getConfig().scraper.endpoints.items,
