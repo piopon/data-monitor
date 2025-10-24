4f7f809ac60af4e2764069adf3d5ea5541c14425
Piotr Ponikowski <piopon.github@gmail.com>
2025-10-24 21:30:33 +0200

    updated notifier API route to handle unsupported values + refactor

--- src/app/api/notifier/route.js
+++ src/app/api/notifier/route.js
@@ -2,12 +2,16 @@ import { AppConfig } from "@/config/AppConfig";
 import { MailNotifier } from "@/notifiers/MailNotifier";
 
 export async function POST(request) {
-  const searchParams = request.nextUrl.searchParams;
-  const notifierType = searchParams.get("type");
-  const notifierData = await request.json();
+  const notifierType = request.nextUrl.searchParams.get("type");
   if (notifierType === "email") {
     var notifier = new MailNotifier(AppConfig.getConfig().notifier.mail);
+  } else {
+    return new Response(`Unsupported notifier type: ${notifierType}`, {
+      status: 400,
+      headers: { "Content-Type": "application/json" },
+    });
   }
+  const notifierData = await request.json();
   const res = await notifier.notify(notifierData);
   return new Response(JSON.stringify(res.info), {
     status: res.result ? 200 : 400,
