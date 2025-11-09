0d980202f188dc076cfd986bf4f5b5aca73f8dac
Piotr Ponikowski <piopon.github@gmail.com>
2025-11-09 12:43:52 +0100

    optimized notifier registry logic using singleton instances map

--- src/notifiers/NotifierRegistry.js
+++ src/notifiers/NotifierRegistry.js
@@ -4,17 +4,21 @@ import { MailNotifier } from "./MailNotifier.js";
 
 export class NotifierRegistry {
   static #CONFIG = AppConfig.getConfig().notifier;
+  static #INSTANCES = new Map();
   static #REGISTRY = {
     MailNotifier,
     DiscordNotifier,
   };
 
   static create(classInfo) {
-    const NotifierClass = NotifierRegistry.#REGISTRY[classInfo.type];
-    if (!NotifierClass) {
-      throw new Error(`Unsupported notifier type: ${type}`);
+    if (!NotifierRegistry.#INSTANCES.has(classInfo.type)) {
+      const NotifierClass = NotifierRegistry.#REGISTRY[classInfo.type];
+      if (!NotifierClass) {
+        throw new Error(`Unsupported notifier type: ${type}`);
+      }
+      const config = NotifierRegistry.#CONFIG[classInfo.config];
+      NotifierRegistry.#INSTANCES.set(classInfo.type, new NotifierClass(config));
     }
-    const config = NotifierRegistry.#CONFIG[classInfo.config];
-    return new NotifierClass(config);
+    return NotifierRegistry.#INSTANCES.get(classInfo.type);
   }
 }
