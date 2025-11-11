105c9736cb0d3e3fd98d8d0e26b545f173b297c1
Piotr Ponikowski <piopon.github@gmail.com>
2025-11-11 18:08:39 +0100

    stopping Next.js application when notifiers are incorrectly configured

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,11 @@
+import { NotifierValidator } from './src/notifiers/NotifierValidator.js';
+
+const notifierCheck = NotifierValidator.validateConfiguration();
+console.log(notifierCheck.info);
+if (!notifierCheck.result) {
+    process.exit(1);
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
