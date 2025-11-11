689e31176de3ecce9a51ece018c4496547242efb
Piotr Ponikowski <piopon.github@gmail.com>
2025-11-11 17:59:38 +0100

    updated validator's logic by comparing application config fields

--- src/notifiers/NotifierValidator.js
+++ src/notifiers/NotifierValidator.js
@@ -1,3 +1,4 @@
+import { AppConfig } from "../config/AppConfig.js";
 import { Notifier } from "./Notifier.js";
 import { NotifierRegistry } from "./NotifierRegistry.js";
 
@@ -5,11 +6,15 @@ export class NotifierValidator {
   static validateConfiguration() {
     const notifiersList = Notifier.getSupportedNotifiers();
     const notifiersRegistry = NotifierRegistry.getNotifiersRegistry();
+    const notifiersConfig = Object.keys(AppConfig.getConfig().notifier);
 
-    if (notifiersList.size !== notifiersRegistry.size) {
-      return {result: false, info: `❌ Notifier sizes mismatch.`};
+    if (notifiersList.size !== notifiersRegistry.size || notifiersList.size !== notifiersConfig.length) {
+      return {result: false, info: `❌ Notifier config sizes mismatch.`};
     }
     for (const [notifierId, className] of notifiersList.entries()) {
+      if (!notifiersConfig.includes(notifierId)) {
+        return {result: false, info: `❌ Missing config entry for notifier "${notifierId}"`};
+      }
       if (!notifiersRegistry.has(className)) {
         return {result: false, info: `❌ Missing backend class "${className}" for notifier "${notifierId}"`};
       }
