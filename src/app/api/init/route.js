3635d5f99199d776f9184cb865725e9a7a892281
Piotr Ponikowski <piopon.github@gmail.com>
2025-11-16 23:31:33 +0100

    calling scraper features retrieval in monitor initialize API method

--- src/app/api/init/route.js
+++ src/app/api/init/route.js
@@ -1,3 +1,5 @@
+import { AppConfig } from "@/config/AppConfig";
+import { ScraperRequest } from "@/lib/ScraperRequest";
 import { MonitorService } from "@/model/MonitorService";
 import { UserService } from "@/model/UserService";
 
@@ -16,7 +18,14 @@ export async function GET(request) {
       headers: { "Content-Type": "application/json" },
     });
   }
-  return new Response(JSON.stringify("Database initialized correctly."), {
+  const featuresResponse = await ScraperRequest.GET(AppConfig.getConfig().scraper.endpoints.features);
+  if (!featuresResponse.ok) {
+    return new Response(JSON.stringify("Cannot get scraper features"), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+  return new Response(JSON.stringify(await featuresResponse.json()), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   });
