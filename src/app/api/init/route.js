946cffc9c9f2c47c533b1de7b9c8dccb0dee3a83
Piotr Ponikowski <piopon.github@gmail.com>
2025-11-17 22:51:17 +0100

    unified result object contents for ALL paths in init API route

--- src/app/api/init/route.js
+++ src/app/api/init/route.js
@@ -6,26 +6,32 @@ import { UserService } from "@/model/UserService";
 export async function GET(request) {
   const userInitResult = await UserService.initializeTable();
   if (!userInitResult.result) {
-    return new Response(JSON.stringify(userInitResult.message), {
+    const result = {init: false, message: userInitResult.message};
+    return new Response(JSON.stringify(result), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
   }
   const monitorInitResult = await MonitorService.initializeTable();
   if (!monitorInitResult.result) {
-    return new Response(JSON.stringify(monitorInitResult.message), {
+    const result = {init: false, message: monitorInitResult.message};
+    return new Response(JSON.stringify(result), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
   }
   const featuresResponse = await ScraperRequest.GET(AppConfig.getConfig().scraper.endpoints.features);
   if (!featuresResponse.ok) {
-    return new Response(JSON.stringify("Cannot get scraper features"), {
+    const result = {init: false, message: "Cannot get scraper features status"};
+    return new Response(JSON.stringify(result), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
   }
-  return new Response(JSON.stringify(await featuresResponse.json()), {
+  const features = await featuresResponse.json();
+  features["init"] = true;
+  features["message"] = "Database initialized correctly.";
+  return new Response(JSON.stringify(features), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   });
