1e1a0be39b069f16705e1ecda6575173507902e9
Piotr Ponikowski <piopon.github@gmail.com>
2025-11-17 23:58:53 +0100

    bugfix: used full init API url to avoid errors

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import HomePage from "@/pages/HomePage";
 import GuestAccess from "@/components/GuestAccess";
 
 export default async function Home() {
-  const response = await fetch("/api/init");
+  const serverAddress = `http://${process.env.SERVER_URL}:${process.env.SERVER_PORT}`;
+  const response = await fetch(`${serverAddress}/api/init`);
   if (!response.ok) {
     const err = await response.json();
     toast.error(err.message);
