3dbfb550a73b30ad650937ac699e5d3dff2e53b5
Piotr Ponikowski <piopon.github@gmail.com>
2025-11-20 23:20:39 +0100

    added logic for retrying data request for demo session

--- src/app/data/page.js
+++ src/app/data/page.js
@@ -8,23 +8,35 @@ import DataCards from "@/components/DataCards";
 import { LoginContext } from "@/context/Contexts";
 
 export default function Data() {
+  const MAX_ATTEMPTS = 5;
+  const WAIT_TIME_MS = 500;
+
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
-  const { token } = useContext(LoginContext);
+  const { isDemo, token } = useContext(LoginContext);
 
   useEffect(() => {
     const getData = async () => {
       if (!token) return;
       try {
-        const response = await fetch("/api/scraper/data", {
-          method: "GET",
-          headers: { Authorization: `Bearer ${token}` },
-        });
-        if (!response.ok) {
-          const message = await response.text();
-          toast.error(message);
-          return;
-        }
+        let attemptNo = 1;
+        do {
+          var response = await fetch("/api/scraper/data", {
+            method: "GET",
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          if (!response.ok) {
+            if (isDemo) {
+              attemptNo++;
+              toast.warn("Waiting for demo to be initialized...");
+              await new Promise(res => setTimeout(res, WAIT_TIME_MS))
+            } else {
+              const message = await response.text();
+              toast.error(message);
+              return;
+            }
+          }
+        } while (!response.ok && isDemo && attemptNo <= MAX_ATTEMPTS);
         const data = await response.json();
         setData(data);
       } catch (error) {
@@ -34,7 +46,7 @@ export default function Data() {
       }
     };
     getData();
-  }, [token]);
+  }, [isDemo, token]);
 
   return (
     <UserAccess>
