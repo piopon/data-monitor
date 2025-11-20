a32bbf0e40d05b180b024ea38a02198cd60b4667
Piotr Ponikowski <piopon.github@gmail.com>
2025-11-20 23:23:55 +0100

    explicitly defining reactStrictMode in Next.js config file

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ if (!notifierCheck.result) {
 }
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+};
 
 export default nextConfig;
