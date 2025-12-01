fd8ecef08bf2f3ad5f05aa51dbd77a96ad9b65d4
Piotr Ponikowski <piopon.github@gmail.com>
2025-12-01 23:57:29 +0100

    reformatted components/EmptyCards.jsx file

--- src/components/EmptyCards.jsx
+++ src/components/EmptyCards.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 const EmptyCards = () => {
   return (
-    <div id='empty-cards-placeholder'>
-        <span className='empty-title'>Ah, fresh start, clean vibes...</span>
-        <p>Go ahead and add your first content to set things in motion!</p>
+    <div id="empty-cards-placeholder">
+      <span className="empty-title">ah, fresh start, clean vibes...</span>
+      <p>perfect moment to add your first content to set things in motion!</p>
     </div>
-  )
-}
+  );
+};
 
-export default EmptyCards
+export default EmptyCards;
