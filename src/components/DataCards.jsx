8b750c3e099f0b49c0f07eb5f9c633c0abc4a5be
Piotr Ponikowski <piopon.github@gmail.com>
2025-12-01 23:36:34 +0100

    added simple differentiation between not-empty/empty data cards

--- src/components/DataCards.jsx
+++ src/components/DataCards.jsx
@@ -3,9 +3,7 @@ import DataCard from "@/components/DataCard";
 const DataCards = ({ data }) => {
   return (
     <div className="data-cards-div">
-      {data.map((item) => (
-        <DataCard key={item.name} data={item} />
-      ))}
+      {data.length > 0 ? data.map((item) => <DataCard key={item.name} data={item} />) : <div>NO CARDS PRESENT</div>}
     </div>
   );
 };
