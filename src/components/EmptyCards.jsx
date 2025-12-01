6d9e6ac84ae784ff58c8f8ce130a02e8567c3f7d
Piotr Ponikowski <piopon.github@gmail.com>
2025-12-01 23:56:11 +0100

    fixed empty cards HTML structure for easier styling

--- src/components/EmptyCards.jsx
+++ src/components/EmptyCards.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const EmptyCards = () => {
   return (
-    <div id='empty-cards-placeholder'>Fresh start, clean vibes. Go ahead and add your first content to set things in motion.</div>
+    <div id='empty-cards-placeholder'>
+        <span className='empty-title'>Ah, fresh start, clean vibes...</span>
+        <p>Go ahead and add your first content to set things in motion!</p>
+    </div>
   )
 }
 
