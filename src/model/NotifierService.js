9a00e27192e1a3080a4ad3b7afaa43ec1b3febb3
Piotr Ponikowski <piopon.github@gmail.com>
2025-12-09 19:19:35 +0100

    added get and filter notifiers logic in NotifierService class

--- src/model/NotifierService.js
+++ src/model/NotifierService.js
@@ -15,4 +15,34 @@ export class NotifierService {
       return { result: false, message: `Cannot initialize '${NotifierService.#DB_TABLE_NAME}' table: ${error.message}.` };
     }
   }
+
+  static async getNotifiers() {
+    const { rows } = await DatabaseQuery(`SELECT * FROM ${NotifierService.#DB_TABLE_NAME}`);
+    return rows;
+  }
+
+  static async filterNotifiers(filters) {
+    const values = [];
+    const conditions = [];
+
+    if (filters.type) {
+      values.push(filters.type);
+      conditions.push(`type = $${values.length}`);
+    }
+    if (filters.origin) {
+      values.push(filters.origin);
+      conditions.push(`origin = $${values.length}`);
+    }
+    if (filters.sender) {
+      values.push(filters.sender);
+      conditions.push(`sender = $${values.length}`);
+    }
+    if (filters.password) {
+      values.push(filters.password);
+      conditions.push(`password = $${values.length}`);
+    }
+    const whereClause = conditions.length > 0 ? "WHERE " + conditions.join(" AND ") : "";
+    const { rows } = await DatabaseQuery(`SELECT * FROM ${NotifierService.#DB_TABLE_NAME} ${whereClause}`, values);
+    return rows;
+  }
 }
