082c2400633dc0e82c1440f35a1ceca0ac7108d9
Piotr Ponikowski <piopon.github@gmail.com>
2025-12-20 00:17:22 +0100

    corrected option label and value usage by providing appropriate mappers

--- src/widgets/Select.jsx
+++ src/widgets/Select.jsx
@@ -4,6 +4,8 @@ const Select = ({ options, value, disabled, setter }) => {
   return (
     <ReactSelect
       options={options}
+      getOptionLabel={(option) => option.text}
+      getOptionValue={(option) => option.value}
       value={options.find(o => o.value === value)}
       isDisabled={disabled}
       onChange={setter}
