7e0d14242d3b6638bde128b32226b947bce8ab58
Piotr Ponikowski <piopon.github@gmail.com>
2025-12-21 17:31:07 +0100

    reogranized notifiers section by wrapping cards in div + new add button

--- src/pages/NotifiersPage.jsx
+++ src/pages/NotifiersPage.jsx
@@ -2,13 +2,22 @@ import NotifierCard from "@/components/NotifierCard";
 import { NotifierCatalog } from "@/notifiers/core/NotifierCatalog";
 
 const NotifiersPage = () => {
+  const addNotifier = (event) => {
+    console.log(event);
+  };
+
   return (
     <section id="notifiers-section">
-      {NotifierCatalog.getSupportedNotifiers()
-        .keys()
-        .map((type) => (
-          <NotifierCard key={type} type={type} />
-        ))}
+      <div className="notifier-cards-div">
+        {NotifierCatalog.getSupportedNotifiers()
+          .keys()
+          .map((type) => (
+            <NotifierCard key={type} type={type} />
+          ))}
+      </div>
+      <button className="add-notifier" onClick={addNotifier}>
+        add
+      </button>
     </section>
   );
 };
