c42bc0bee4e7b73b63ce47826fbd8924f568715c
Piotr Ponikowski <piopon.github@gmail.com>
2025-12-22 00:23:13 +0100

    used internal notifier array (to be loaded with backend data) to show configured notifiers

--- src/pages/NotifiersPage.jsx
+++ src/pages/NotifiersPage.jsx
@@ -1,19 +1,18 @@
+import { useState } from "react";
 import NotifierCard from "@/components/NotifierCard";
-import { NotifierCatalog } from "@/notifiers/core/NotifierCatalog";
 
 const NotifiersPage = () => {
-  const addNotifier = (event) => {
-    console.log(event);
+  const [notifiers, setNotifiers] = useState([]);
+
+  const addNotifier = () => {
+    notifiers.push("test");
+    setNotifiers(notifiers);
   };
 
   return (
     <section id="notifiers-section">
       <div className="notifier-cards-div">
-        {NotifierCatalog.getSupportedNotifiers()
-          .keys()
-          .map((type) => (
-            <NotifierCard key={type} type={type} />
-          ))}
+        {notifiers.map((type) => <NotifierCard key={type} type={type} />)}
       </div>
       <button className="add-notifier" onClick={addNotifier}>
         add
