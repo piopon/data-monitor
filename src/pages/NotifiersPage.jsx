330eb6140008165af61bff263ee87d224d37bf12
Piotr Ponikowski <piopon.github@gmail.com>
2025-12-22 20:37:57 +0100

    addet initialize notifiers page logic by getting current notifiers array

--- src/pages/NotifiersPage.jsx
+++ src/pages/NotifiersPage.jsx
@@ -1,9 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NotifierCard from "@/components/NotifierCard";
 import EmptyCards from "@/components/EmptyCards";
 
 const NotifiersPage = () => {
   const [notifiers, setNotifiers] = useState([]);
+
+  useEffect(() => {
+    const initialize = async () => {
+      try {
+        const notifiersResponse = await fetch(`/api/notifier`);
+        const notifiersData = await notifiersResponse.json();
+        if (!notifiersResponse.ok) {
+          toast.error(notifiersData.message);
+          return;
+        }
+        if (0 === notifiersData.length) {
+          return;
+        }
+        setNotifiers(notifiersData);
+      } catch (error) {
+        toast.error(`Failed to get notifier data: ${error.message}`);
+      }
+    };
+    initialize();
+  }, []);
 
   const addNotifier = () => {
     setNotifiers((currentNotifiers) => [...currentNotifiers, "test"]);
