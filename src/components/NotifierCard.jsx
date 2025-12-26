b3def83a3238d86e2c5bffba431e97d0789a254e
Piotr Ponikowski <piopon.github@gmail.com>
2025-12-26 14:48:49 +0100

    creating empty/fresh UI to first select notifier type then show settings

--- src/components/NotifierCard.jsx
+++ src/components/NotifierCard.jsx
@@ -1,8 +1,21 @@
+import { NotifierCatalog } from "@/notifiers/core/NotifierCatalog";
+import Select from "@/widgets/Select";
+
 const NotifierCard = ({ data }) => {
+  const createEmpty = () => {
+    return <Select options={NotifierCatalog.getSupportedNotifiers().keys()} disabled={false} />;
+  };
+
+  const createUI = () => {
+    if (!data.type) {
+      return createEmpty();
+    }
+    return <div className="notifier-card-items">setting</div>;
+  }
+
   return (
     <div className="notifier-card">
-      <h3 className="notifier-card-title">{data.type}</h3>
-      <div className="notifier-card-items">setting</div>
+      {createUI()}
     </div>
   );
 };
