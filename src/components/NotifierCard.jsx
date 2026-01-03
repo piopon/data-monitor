618c7715f4e42db8c15a9d6ebbb706807fb7d01d
Piotr Ponikowski <piopon.github@gmail.com>
2026-01-03 21:35:53 +0100

    bugfix: added missing toast import statement

--- src/components/NotifierCard.jsx
+++ src/components/NotifierCard.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { toast } from "react-toastify";
 import { NotifierCatalog } from "@/notifiers/core/NotifierCatalog";
 import Select from "@/widgets/Select";
 
