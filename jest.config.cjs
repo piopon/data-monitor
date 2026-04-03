f874479d7c4dcaf8a80b891d0188995bdff91a21
Piotr Ponikowski <piopon.github@gmail.com>
2026-04-03 13:58:25 +0200

    added and configured Jest test framework + first new UI test for Select widget

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const customJestConfig = {
+  testEnvironment: "jest-environment-jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testMatch: ["<rootDir>/tests/**/*.test.jsx"],
+};
+
+module.exports = createJestConfig(customJestConfig);
