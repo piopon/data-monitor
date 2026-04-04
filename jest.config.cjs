9136d2da9be625fad184c0485a1ecc3be32f4199
Piotr Ponikowski <piopon.github@gmail.com>
2026-04-04 11:39:27 +0200

    added coverage report to the unit tests run

--- jest.config.cjs
+++ jest.config.cjs
@@ -8,6 +8,9 @@ const customJestConfig = {
   testEnvironment: "jest-environment-jsdom",
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
   testMatch: ["<rootDir>/tests/**/*.test.js", "<rootDir>/tests/**/*.test.jsx"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.{js,jsx}"],
+  coverageDirectory: "<rootDir>/coverage",
+  coverageReporters: ["text-summary", "lcov", "json-summary"],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/src/$1",
   },
