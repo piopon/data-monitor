9e694f824a80e3204d34707d95681a62a3f4daba
Piotr Ponikowski <piopon.github@gmail.com>
2026-04-14 19:04:24 +0200

    updated login provider logic to store demo session based on explicit settings & localStorage

--- src/context/LoginProvider.jsx
+++ src/context/LoginProvider.jsx
@@ -4,14 +4,12 @@ import { useEffect, useState } from "react";
 import { LoginContext } from "@/context/Contexts";
 
 const LoginProvider = ({ children }) => {
-  const DEMO_USER_ID = 7357;
-
   const [id, setId] = useState(-1);
   const [challenge, setChallenge] = useState(null);
   const [token, setToken] = useState(null);
   const [email, setEmail] = useState(null);
+  const [isDemo, setIsDemo] = useState(false);
   const [authReady, setAuthReady] = useState(false);
-  const isDemo = DEMO_USER_ID === id;
   const userLogged = authReady && !!token;
 
   useEffect(() => {
@@ -19,26 +17,32 @@ const LoginProvider = ({ children }) => {
     if (storedToken) {
       setToken(storedToken);
     }
+    const storedDemo = localStorage.getItem("isDemo");
+    setIsDemo(storedDemo === "true");
     setAuthReady(true);
   }, []);
 
-  const login = (id, email, data) => {
+  const login = (id, email, data, demoSession = false) => {
     setId(id);
     setEmail(email);
     setChallenge(data.challenge);
     setToken(data.token);
+    setIsDemo(demoSession);
     localStorage.setItem("id", id);
     localStorage.setItem("token", data.token);
+    localStorage.setItem("isDemo", demoSession ? "true" : "false");
   };
 
-  const demo = (data) => login(DEMO_USER_ID, null, data);
+  const demo = (id, email, data) => login(id, email, data, true);
 
   const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("id");
+    localStorage.removeItem("isDemo");
     setToken(null);
     setChallenge(null);
     setEmail(null);
+    setIsDemo(false);
     setId(-1);
   };
 
