5c3a350640472ddf1383c0c9ae1df6c541419067
Piotr Ponikowski <piopon.github@gmail.com>
2026-04-23 23:50:25 +0200

    move react-select inline styles into centralized CSS selectors

--- src/widgets/Select.jsx
+++ src/widgets/Select.jsx
@@ -1,46 +1,10 @@
 import ReactSelect from "react-select";
 
-const CONTROL_HEIGHT = 44;
-
-const SELECT_STYLES = {
-  control: (base) => ({
-    ...base,
-    minHeight: CONTROL_HEIGHT,
-    height: CONTROL_HEIGHT,
-  }),
-  valueContainer: (base) => ({
-    ...base,
-    height: CONTROL_HEIGHT,
-    padding: "0 6px",
-  }),
-  indicatorsContainer: (base) => ({
-    ...base,
-    height: CONTROL_HEIGHT,
-  }),
-  indicatorSeparator: () => ({
-    display: "none",
-  }),
-  input: (base) => ({
-    ...base,
-    margin: 0,
-    padding: 0,
-  }),
-  singleValue: (base) => ({
-    ...base,
-    margin: 0,
-  }),
-  placeholder: (base) => ({
-    ...base,
-    margin: 0,
-  }),
-};
-
 const Select = ({ size = "small", options, placeholder="select...", value, disabled, setter }) => {
   return (
     <ReactSelect
       className={`select-${size}`}
       classNamePrefix="react-select"
-      styles={SELECT_STYLES}
       options={options}
       placeholder={placeholder}
       getOptionLabel={(option) => option.text}
